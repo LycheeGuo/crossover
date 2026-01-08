179ef7c282e7aa9d3582e602ea488a24f7b4f76f
Lizhi Guo <guolz199702@gmail.com>
2026-01-08 00:22:36 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ const SOCKS5_WHITELIST_DEFAULT = ['*tapecontent.net', '*cloudatacdn.com', '*load
 
 // [配置] 谷歌学术专用代理池 (硬编码，负载均衡)
 const GOOGLE_SCHOLAR_PROXIES = [
-    'http://45.56.228.8:443'
+    'http://51.68.192.76:14443'
 ];
 
 // [变量] 模块级变量 (仅用于缓存配置)
@@ -1531,6 +1531,7 @@ async function html1101(host, 访问IP) {
 </body>
 </html>`;
 }
+
 
 
 
